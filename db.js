6f0daabdb19cb34fa628327330250c7d5ff6622b
Edwin Irungu <irungundigirigi@gmail.com>
2025-05-26 13:24:58 +0300

    minor fix: import dotenv

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import pg from 'pg';
 const {Pool} = pg;
+
+console.log('Connecting with:', process.env.DATABASE_URL);
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL,   idleTimeoutMillis: 30000,
     keepAlive: true });
