6964da85d99c46f465de098b368744219a6184cf
Edwin Irungu <irungundigirigi@gmail.com>
2025-05-26 13:22:18 +0300

    minor change pool-connect

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -5,8 +5,9 @@ import pool from '../db.js';
 class AuthController {
   static async login(req, res) {
     const { email, password } = req.body;
+    const client = await pool.connect()
     try {
-      const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+      const result = await client.query('SELECT * FROM users WHERE email = $1', [email]);
       if (result.rows.length === 0)
         return res.status(401).json({ message: 'User not found' });
 
